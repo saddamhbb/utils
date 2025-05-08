59e11a4430528ee854565f99fe2158b945428236
saddamhbb <promised.bluesky@gmail.com>
2025-05-08 14:05:46 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
-export function capitalize(str: string): string {
-    if (typeof str !== 'string') return ''
-    return str.charAt(0).toUpperCase() + str.slice(1)
-}
-
 export function hyphenate(str) {
   if (typeof str !== 'string') return ''
   return str.split('').join('-')
